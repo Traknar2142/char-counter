8192f4abae70d08f25d5ab3efece815cf21ac57c
Traknar <Traknar@DESKTOP-3M51OS8>
2020-07-29 18:09:51 +0300

    first commit

---
+++ CountingLetters/src/main/java/ua/com/foxminded/task5/CountingLettersApp.java
@@ -0,0 +1,10 @@
+package ua.com.foxminded.task5;
+
+public class CountingLettersApp {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ CountingLetters/src/main/java/ua/com/foxminded/task5/countingletters/CountingLetters.java
@@ -0,0 +1,5 @@
+package ua.com.foxminded.task5.countingletters;
+
+public class CountingLetters {
+
+}
