a07fdee17950b29742f471f035c27fb9cbd805c4
Traknar <Traknar@DESKTOP-3M51OS8>
2020-08-09 13:43:08 +0300

    Rewrite printUniqueCharacters method Remove removeDublicateCharacters method

--- CountingLetters/src/main/java/ua/com/foxminded/task5/charcounter/CharCounter.java
+++ CountingLetters/src/main/java/ua/com/foxminded/task5/charcounter/CharCounter.java
@@ -2,44 +2,33 @@ package ua.com.foxminded.task5.charcounter;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 
 public class CharCounter {
     private Map<String, String> cache = new HashMap<>();
 
-    public String printUniqueCharacters(String word) {            
-            List<String> inputWord = Arrays.asList(word);
-            List<String> outputString = inputWord
-                    .stream()
-                    .map(wordFromInput -> cache.containsKey(wordFromInput) ? cache.get(wordFromInput) : makeOutputString(wordFromInput))
-                    .collect(Collectors.toList());
-            String result = outputString.toString();
-            return result.substring(1,outputString.toString().length()-2);
-        }    
+    public String printUniqueCharacters(String word) {
+        String st1 = Optional.ofNullable(cache.get(word)).orElseGet(() -> makeOutputString(word));
 
-    private Character[] removeDublicateCharacters(Character[] letters) {
-        Set<Character> setOfCharWithoutIteration = new LinkedHashSet<>();
-
-        Collections.addAll(setOfCharWithoutIteration, letters);
-        Character[] uniqueLetters = setOfCharWithoutIteration.toArray(new Character[0]);
-        return uniqueLetters;
+        return st1.substring(0, st1.toString().length() - 1);
     }
-    
+
     private String makeOutputString(String word) {
         StringBuilder result = new StringBuilder();
-        char[] letters = word.toCharArray();
-        Character[] uniqueLetters = removeDublicateCharacters(ArrayUtils.toObject(letters));
-        
+
+        List<Character> inputWord = word
+                .chars()
+                .mapToObj(letter -> Character.valueOf((char) letter))
+                .collect(Collectors.toList());
+        Set<Character> setOfCharWithoutIteration = new LinkedHashSet<>(inputWord);
+        Map<Character, Long> numbersOfOccurrences = inputWord
+                .stream()
+                .collect(Collectors.groupingBy(letter -> letter, Collectors.counting()));
+
         result.append(word + "\n");
-        
-        Stream<Character> streamOfUniqueCharacter = Arrays.stream(uniqueLetters);
-        streamOfUniqueCharacter.forEach(letter ->                
-            result.append("\"" + letter + "\"" + " - " + StringUtils.countMatches(word,letter) + "\n")
-        ); 
-        cache.put(word, result.toString());        
+        setOfCharWithoutIteration.forEach(
+                letter -> result.append("\"" + letter + "\"" + " - " + numbersOfOccurrences.get(letter) + "\n"));
+
+        cache.put(word, result.toString());
         return result.toString();
-    }   
+    }
 }
